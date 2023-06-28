0de86eebef803eef02c97ffe60d95a6283437dbe
Gabriel Lagrotteria <gabriellagrotteria18@gmail.com>
2023-06-28 16:17:02 -0400

    Insertado el header bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.css'
+  import './App.css'
+import {  Header } from './components/header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-  
-    <h1>Hola</h1>
+    <header>
+    < Header />
+
+      <h1>Hola</h1> 
+    </header>
+
   )
 }
 
