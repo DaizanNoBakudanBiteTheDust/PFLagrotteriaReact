074bea5ea501a7069d1b16d299205ce24b4397b3
Gabriel Lagrotteria <gabriellagrotteria18@gmail.com>
2023-07-15 22:47:54 -0400

    Se agrego Slint y la api de las cartas pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import 'bootstrap/dist/css/bootstrap.css'
 import './App.css'
 import {  Navbar } from './components/header/navbar'
 import {ItemListContainer} from './components/itemListContainer'
+import pokemon from 'pokemontcgsdk'
+pokemon.configure({apiKey: 'f2c0bdbf-4664-4433-b770-a2f3c0d19e44'})
 
 function App() {
 
