1ef0f4df23e343a76176d0da648129710a70b4dd
Gabriel Lagrotteria <gabriellagrotteria18@gmail.com>
2023-07-16 20:04:04 -0400

    Se agregaron el hook y el renderprops del fetch

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import 'bootstrap/dist/css/bootstrap.css'
 import './App.css'
 import {  Navbar } from './components/header/navbar'
 import {ItemListContainer} from './components/itemListContainer'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import pokemon from 'pokemontcgsdk'
 pokemon.configure({apiKey: 'f2c0bdbf-4664-4433-b770-a2f3c0d19e44'})
 
 function App() {
 
   return (
-<div>
+<BrowserRouter>
+
     <Navbar />
     <ItemListContainer />
-</div>
+
+</BrowserRouter>
 
   )
 }
