64a44bdafb119349df9ddfc6df3b5934e1346289
Gabriel Lagrotteria <gabriellagrotteria18@gmail.com>
2023-07-19 01:55:32 -0400

    Se agrego una estructura al detalle del producto

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,13 +1,18 @@
 const ItemDetail = ({item}) => {
 
     return (
-        <div className="container my-5">
+        <div className="container">
+            <div className="row">
             <h2>{item.nombre}</h2>
+            <div className="col-md-6">
             <img src={item.img} alt={item.nombre}/>
+            </div>
+            <div className="col-md-6">
             <p>{item.descripcion}</p>
             <p>Precio: ${item.precio}</p>
-
             <button className="btn btn-success">Agregar</button>
+            </div>
+            </div>
         </div>
     )
 }
