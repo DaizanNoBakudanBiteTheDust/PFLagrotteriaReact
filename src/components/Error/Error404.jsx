b66d856d2f89ac5b6c201abc910e1ee559c9bb00
Gabriel Lagrotteria <gabriellagrotteria18@gmail.com>
2023-07-19 12:11:09 -0400

    Agregado estilo a error 404

--- src/components/Error/Error404.jsx
+++ src/components/Error/Error404.jsx
@@ -1,8 +1,11 @@
 
+import "./Error404.css"
+
 export const Error = () => {
 
     return (
-        <div>
+        <div className="contenedorError">
+            <img src="https://i.gifer.com/fetch/w300-preview/49/49d92a54362245fa554651b9bc6c1053.gif"/>
         <h2>Error 404</h2>
         <p>Pagina No Encontrada</p>
         </div>
