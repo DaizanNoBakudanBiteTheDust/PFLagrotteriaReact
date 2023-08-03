7fc1509be6dd244ea848432da7b431acb3b46d32
Gabriel Lagrotteria <gabriellagrotteria18@gmail.com>
2023-08-02 23:41:30 -0400

    Se modifico ruta

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import './App.css'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import { Navbar } from './components/header/navbar'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import {ItemDetailContainer} from './components/ItemDetailContainer/ItemDetailContainer'
@@ -7,7 +8,7 @@ import {Error} from './components/Error/Error404'
 import { Footer } from './components/Footer/Footer'
 import ProductosSellados from './components/ProductosSellados/ProductosSellados'
 import { PaginaEnConstruccion } from './components/PaginaEnConstruccion/PaginaEnConstruccion'
-import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+
 
 function App() {
   
